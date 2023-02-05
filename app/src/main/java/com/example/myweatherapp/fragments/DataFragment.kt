6f92bfc547e7a75cd9958e2a79b342c48d597074
Max <max.samurai.04@gmail.com>
2023-02-05 15:28:27 +0200

    done everything

--- app/src/main/java/com/example/myweatherapp/fragments/DataFragment.kt
+++ app/src/main/java/com/example/myweatherapp/fragments/DataFragment.kt
@@ -1,16 +1,62 @@
 package com.example.myweatherapp.fragments
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.location.Address
+import android.location.Geocoder
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
 import com.example.myweatherapp.R
 import com.example.myweatherapp.base.DataBindingBaseFragment
 import com.example.myweatherapp.databinding.FragmentDataBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class DataFragment : DataBindingBaseFragment<FragmentDataBinding, DataFragmentViewModel>() {
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    @SuppressLint("MissingPermission")
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // Precise location access granted.
+                fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                    val addresses: List<Address>? =
+                        it?.latitude?.let { it1 -> it.longitude.let { it2 ->
+                            geocoder.getFromLocation(it1,
+                                it2, 1)
+                        } }
+                    binding.editCityName.setText(addresses?.get(0)?.locality)
+                    viewModel.refreshData(binding.editCityName.text.toString())
+                }
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // Only approximate location access granted.
+            } else -> {
+            // No location access granted.
+        }
+        }
+    }
+
+    private var myLat: Double ?= 0.0
+    private var myLon: Double ?= 0.0
+    private lateinit var geocoder: Geocoder
+
     override fun getLayoutResId() = R.layout.fragment_data
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        geocoder = Geocoder(requireActivity(), Locale.ENGLISH)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+    }
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewModel(viewModel: DataFragmentViewModel) {
@@ -26,6 +72,9 @@ class DataFragment : DataBindingBaseFragment<FragmentDataBinding, DataFragmentVi
                 textLon.text = it?.coord?.lon.toString()
                 textDegree.text = (it?.main?.temp?.minus(273)?.toInt()).toString() + "°C"
 
+                myLat = it?.coord?.lat
+                myLon = it?.coord?.lon
+
                 Glide.with(requireActivity())
                     .load("https://openweathermap.org/img/wn/" + it?.weather?.get(0)?.icon + "@2x.png")
                     .into(imageWeatherPictures)
@@ -34,6 +83,12 @@ class DataFragment : DataBindingBaseFragment<FragmentDataBinding, DataFragmentVi
 
         binding.imageSearchCity.setOnClickListener {
             viewModel.refreshData(binding.editCityName.text.toString())
+        }
+
+        binding.btnMyPosition.setOnClickListener {
+            locationPermissionRequest.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION))
         }
     }
 }
